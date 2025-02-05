e94c00a622c26bdc6312e0b039b1db28ea28ff51
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2025-02-05 12:00:01 +0100

    first commit

---
+++ app/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='Home w-screen min-h-screen grid justify-center items-center'>
+        <h1 className='text-4xl'>riko NBA</h1>
+    </div>
+  )
+}
+
+export default Home;
